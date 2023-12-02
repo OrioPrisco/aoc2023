9bad64e613a411828ade40ac51d0c2704d6345dc
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-12-02 10:59:26 +0100

    Change ball to cube
    seems like i hallucinated that we were using balls in day 2 lol

--- day02/part1.py
+++ day02/part1.py
@@ -2,7 +2,7 @@ file = open("input")
 
 numbers=[]
 idgame = 1
-max_balls = {
+max_cubes = {
 	"red" : 12,
 	"green" : 13,
 	"blue" : 14,
@@ -12,21 +12,21 @@ for line in file:
 	games = line.split(";")
 	too_many = 0
 	for game in games:
-		total_balls = {
+		total_cubes = {
 			"red" : 0,
 			"green" : 0,
 			"blue" : 0,
 		}
-		balls = game.split(",")
-		for ball in balls:
-			number, color = ball.split()
-			total_balls[color] += int(number)
-		for ball_type in total_balls.keys():
-			if total_balls[ball_type] > max_balls[ball_type]:
+		cubes = game.split(",")
+		for cube in cubes:
+			number, color = cube.split()
+			total_cubes[color] += int(number)
+		for cube_type in total_cubes.keys():
+			if total_cubes[cube_type] > max_cubes[cube_type]:
 				too_many = 1
 	if not too_many:
 		numbers.append(idgame)
-	print(idgame, total_balls)
+	print(idgame, total_cubes)
 	idgame += 1
 print(sum(numbers))
 
--- day02/part2.py
+++ day02/part2.py
@@ -2,7 +2,7 @@ file = open("input")
 
 numbers=[]
 for line in file:
-	total_balls = {
+	total_cubes = {
 		"red" : 0,
 		"green" : 0,
 		"blue" : 0,
@@ -10,13 +10,13 @@ for line in file:
 	line = line.split(":")[1]
 	games = line.split(";")
 	for game in games:
-		balls = game.split(",")
-		for ball in balls:
-			number, color = ball.split()
-			total_balls[color] = max(int(number), total_balls[color])
+		cubes = game.split(",")
+		for cube in cubes:
+			number, color = cube.split()
+			total_cubes[color] = max(int(number), total_cubes[color])
 	power = 1
-	for key in total_balls:
-		power *= total_balls[key]
+	for key in total_cubes:
+		power *= total_cubes[key]
 	numbers.append(power)
 print(sum(numbers))
 
